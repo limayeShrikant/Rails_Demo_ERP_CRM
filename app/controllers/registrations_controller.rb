56ec3ced0b5d7c23abf3b011f4b25269fe097e5b
Shrikant <shrikant.limaye89@gmail.com>
2025-01-14 10:35:15 +0530

    Add edit and signout page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
-RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
 
-private
+  private
 
-def sign_up_params
-  params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation)
-end
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation)
+  end
 
-def account_update_params
-  params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation, :current_password)
-end
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation, :current_password)
+  end
 
 end
